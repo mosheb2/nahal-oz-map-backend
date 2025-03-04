b1f2cd5979248254d13339d6d727858cbe2161a6
shmulik <shmulik.kuperman@ex.co>
2025-03-04 19:53:34 +0200

    fix(): dynamically set where the tooltip will be opened right or left and top or bottom

--- src/client/src/components/map/InteractiveMap.tsx
+++ src/client/src/components/map/InteractiveMap.tsx
@@ -5,6 +5,21 @@ import { MapItem } from "../../map-item/MapItem";
 
 const mapImage = 'https://api.standwithnahaloz.com/assets/nahal_oz_map-min.png';
 
+
+function getTooltipPositionClass(proj: Hotspot) {
+    let posClassname = '';
+
+    if (proj.coordinates.y < 50) {
+        posClassname += 'bottom'
+    }
+
+    if (proj.coordinates.x > 50) {
+        posClassname += ' left';
+    }
+
+    return posClassname;
+}
+
 export function InteractiveMap ({ projects }:{ projects:Hotspot[]} ) {
     return (
         <div id="map">
@@ -13,7 +28,7 @@ export function InteractiveMap ({ projects }:{ projects:Hotspot[]} ) {
                     {projects.map((proj: Hotspot) => {
                         return (
                             <div key={proj._id} className="map-marker" style={{top: `${proj.coordinates.y}%`, left: `${proj.coordinates.x}%`}}>
-                                <div className="marker-campaign bottom">
+                                <div className={`marker-campaign ${getTooltipPositionClass(proj)}`}>
                                     <MapItem {...proj}/>
                                 </div>
                             </div>

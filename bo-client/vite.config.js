83b3b9bfc59a926bd2511908e3cd92b1b4f912e3
shmulik <shmulik.kuperman@ex.co>
2025-03-09 09:04:11 +0200

    feat(): serve all clients from the dist folder, each in his own folder

--- bo-client/vite.config.js
+++ bo-client/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // Import the 'path' module
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: path.resolve(__dirname, '../public/dist/bo-client'), // Change 'your-desired-folder'
+    emptyOutDir: true, // Optional: Clear the output directory before build
+  },
 })
